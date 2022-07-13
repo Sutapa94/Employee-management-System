bcaf415419d2c67379b78040b0b964087461ce03
Sutapa94 <sutapa.banerjee1994@gmail.com>
2022-07-13 17:57:05 +0530

    Custom Pipe(Search Filter) added

---
+++ src/app/Pipe/filter.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterText: string, name:string): any {
+    if(value.length===0 || filterText===''){
+      return value;
+    }
+    else{
+      const result = []
+      for( const item of value){
+        if(item[name].includes(filterText)){
+          result.push(item)
+        }
+      }
+      return result;
+    }
+  }
+
+}
